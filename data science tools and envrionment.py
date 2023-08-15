85a9b955cfbb2555210d3e6d6d76fad1f9417187
UsamaArif406 <141055551+UsamaArif406@users.noreply.github.com>
2023-08-14 21:28:34 -0700

    Add files via upload

---
+++ data science tools and envrionment.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools
+
+# In this specific notebook I will summarize data science tools and its environemnt 
+
+# ### Data Science Languages
+# 1. Python
+# 2. R
+# 3. Julia
+# 4. SQL
+# 5. Scala
+# 
+
+# 1. **NumPy**: Fundamental package for scientific computing with Python.
+# 2. **Pandas**: Powerful library for data manipulation and analysis.
+# 3. **Matplotlib**: Versatile library for creating visualizations.
+# 4. **Seaborn**: Higher-level interface for statistical visualizations.
+# 5. **Scikit-learn**: Machine learning library for data mining and analysis.
+# 6. **SciPy**: Library for mathematics, science, and engineering.
+# 7. **Statsmodels**: Library for estimating and interpreting statistical models.
+# 8. **NLTK**: Toolkit for natural language processing tasks.
+# 9. **TensorFlow**: Machine learning framework developed by Google.
+# 10. **PyTorch**: Machine learning framework developed by Facebook AI Research.
+# 11. **Keras**: High-level interface for building neural networks.
+# 12. **XGBoost**: Optimized gradient boosting library.
+# 13. **LightGBM**: Efficient gradient boosting framework.
+# 14. **CatBoost**: Gradient boosting library for categorical features.
+# 15. **Plotly**: Library for interactive and dynamic visualizations.
+# 
+
+# |Data science Tools|
+# |---|
+# |SAS|
+# |APACHE HADOOP|
+# |TENSORFLOW|
+
+# # Arithematic expressions in Python
+
+# In[1]:
+
+
+a=3
+b=5
+Addition=a+b
+print(Addition)
+
+
+# # Create a code cell to multiply and add numbers
+
+# In[2]:
+
+
+#Multiplication and addition
+mul=(a+b)*a
+print(mul)
+
+
+# # Create a code cell to convert minutes to hours
+
+# In[3]:
+
+
+minutes=3600
+hours=minutes/60
+print('hours ',hours)
+
+
+# # -Insert a markdown cell to list Objectives. 
+
+# __Objectives__
+# * list famous languages for data sceince 
+# * list famous data science libraries
+# * list of data science tools
+# * how to write comments in the cell
+
+# # Create a markdown cell to indicate the Author’s name.
+
+# Usama Arif
+
+# In[ ]:
+
+
+
+
